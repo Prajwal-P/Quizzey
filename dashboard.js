c4dace8844b259605b7a7be143accb05b92f0dd6
Prajwal-P <prajwal72p@gmail.com>
2020-12-13 20:07:39 +0530

    Authentication and Authorization done

--- dashboard.js
+++ dashboard.js
@@ -1,40 +1,66 @@
+let baseURL = 'http://127.0.0.1:8888';
+
 let all_classrooms = [
 	{
 		id: 1,
-		name: "Web Technology",
-		description: "Subject Code : 17CS71, Teacher : Sushma H R, Classroom for taking quiz related to HTML, CSS and JS"
+		name: 'Web Technology',
+		description:
+			'Subject Code : 17CS71, Teacher : Sushma H R, Classroom for taking quiz related to HTML, CSS and JS'
 	},
 	{
 		id: 2,
-		name: "Advanced Computer Architecture",
-		description: "Subject Code : 17CS72, Teacher : Hemavathi, Classroom for taking quiz related to ACA"
+		name: 'Advanced Computer Architecture',
+		description:
+			'Subject Code : 17CS72, Teacher : Hemavathi, Classroom for taking quiz related to ACA'
 	},
 	{
 		id: 3,
-		name: "ML",
-		description: "Subject Code : 17CS73, Teacher : Asha T"
+		name: 'ML',
+		description: 'Subject Code : 17CS73, Teacher : Asha T'
 	},
 	{
 		id: 4,
-		name: "UNIX",
-		description: "Teacher : Divyashree, Classroom for taking quiz related to unix"
+		name: 'UNIX',
+		description:
+			'Teacher : Divyashree, Classroom for taking quiz related to unix'
 	},
 	{
 		id: 5,
-		name: "SAN",
-		description: "Teacher : Nikhitha"
+		name: 'SAN',
+		description: 'Teacher : Nikhitha'
 	},
 	{
 		id: 6,
-		name: "Web Technology lab and Mini-project",
-		description: ""
+		name: 'Web Technology lab and Mini-project',
+		description: ''
 	},
 	{
 		id: 7,
-		name: "ML LAb",
-		description: "Teacher : Madhuri, Classroom for taking quiz related to ML lab"
+		name: 'ML LAb',
+		description:
+			'Teacher : Madhuri, Classroom for taking quiz related to ML lab'
 	}
-]
+];
+
+window.onload = () => {
+	var requestOptions = {
+		method: 'GET',
+		mode: 'cors',
+		credentials: 'include'
+	};
+
+	fetch(`${baseURL}/user/check`, requestOptions)
+		.then(result => result.json())
+		.then(res => {
+			// console.log(res);
+			if (res.STATUS === 1);
+			else window.location = 'signIn.html';
+			document.getElementById('name').innerHTML = sessionStorage.getItem(
+				'name'
+			);
+		})
+		.catch(error => console.log('error', error));
+};
 
 function cls_template(cls) {
 	return `
@@ -45,46 +71,65 @@ function cls_template(cls) {
 				${cls.description}
 			</span>
 		</div>
-	`
+	`;
 }
 
 document.querySelector('.class_card_wrapper').innerHTML = `
 	${all_classrooms.map(cls_template).join('')}
-`
+`;
 
 let menu_visible = false;
 function toggle_dropdown() {
 	// let dd = document.getElementById("dropdown");
-	let dd = document.querySelector('.dd_wrapper')
+	let dd = document.querySelector('.dd_wrapper');
 	if (menu_visible) {
-		dd.classList.remove("show");
+		dd.classList.remove('show');
 		menu_visible = false;
 	} else {
-		dd.classList.add("show");
+		dd.classList.add('show');
 		menu_visible = true;
 	}
 }
 
 let modal1_visible = false;
 function toggle_modal_1() {
-	let modal = document.querySelector('.modal-bg')
+	let modal = document.querySelector('.modal-bg');
 	if (modal1_visible) {
 		modal.classList.remove('show-modal1');
-		modal1_visible = false
+		modal1_visible = false;
 	} else {
 		modal.classList.add('show-modal1');
-		modal1_visible = true
+		modal1_visible = true;
 	}
 }
 
 let modal2_visible = false;
 function toggle_modal_2() {
-	let modal = document.querySelector('.modal-bg')
+	let modal = document.querySelector('.modal-bg');
 	if (modal2_visible) {
 		modal.classList.remove('show-modal2');
-		modal2_visible = false
+		modal2_visible = false;
 	} else {
 		modal.classList.add('show-modal2');
-		modal2_visible = true
+		modal2_visible = true;
 	}
 }
+
+let signOut = () => {
+	sessionStorage.clear();
+
+	var requestOptions = {
+		method: 'GET',
+		mode: 'cors',
+		credentials: 'include'
+	};
+	fetch(`${baseURL}/user/signOut`, requestOptions)
+		.then(result => result.json())
+		.then(res => {
+			alert(res.MESSAGE);
+			if (res.STATUS === 1) {
+				window.location = 'signIn.html';
+			}
+		})
+		.catch(error => console.log('error', error));
+};
--- server/Routes/user.js
+++ server/Routes/user.js
@@ -5,12 +5,13 @@ const upload = multer();
 const mysqlConnection = require('../connection');
 
 const sendRes = require('../controllers/sendRes');
+const check = require('../middleware/checkUser.js');
 
 const router = express.Router();
 
 let sql;
 
-router.post('/signup', upload.none(), function (req, res) {
+router.post('/signUp', upload.none(), (req, res) => {
 	const { first_name, last_name, email, password } = req.body;
 	if (first_name && last_name && email && password) {
 		bcrypt.hash(password, 10, (err, hash) => {
@@ -48,6 +49,134 @@ router.post('/signup', upload.none(), function (req, res) {
 	} else {
 		sendRes(0, res, undefined, 'INSUFFICIENT DATA');
 	}
+});
+
+router.post('/signIn', upload.none(), (req, res) => {
+	const { email, password } = req.body;
+
+	const _userId = req.cookies.userId;
+	const _authToken = req.cookies.authToken;
+	let userId, userName;
+
+	const fetchUser = () => {
+		if (email && password) {
+			sql = `SELECT ID, FIRST_NAME, PASSWORD FROM TBL_USER WHERE EMAIL='${email}'`;
+			mysqlConnection.query(sql, (e, r) => checkUser(e, r));
+		} else {
+			sendRes(0, res, undefined, 'INSUFFICIENT DATA');
+		}
+	};
+
+	const checkUser = (err, result) => {
+		if (err) {
+			console.log(err);
+		} else {
+			if (result.length === 0) {
+				sendRes(0, res, undefined, 'INVALID USERNAME OR PASSWORD');
+			} else {
+				userId = result[0]['ID'];
+				userName = result[0]['FIRST_NAME'];
+				// console.log('User ID: ', userId);
+				bcrypt.compare(password, result[0]['PASSWORD'], (e, r) =>
+					checkPassword(e, r)
+				);
+			}
+		}
+	};
+
+	const checkPassword = (err, result) => {
+		if (result) {
+			// userId = result[0]['USER_ID'];
+			// console.log('User ID: ', userId);
+			if (userId !== parseInt(_userId)) {
+				console.log('Logging in new user...');
+			}
+			if (_authToken) {
+				// console.log('Query with auth token');
+				sql = `SELECT VALIDITY FROM TBL_USER_AUTH WHERE USER_ID='${userId}' AND AUTH_ID='${_authToken}';`;
+			} else {
+				// console.log('Query w/o auth token');
+				sql = `SELECT VALIDITY FROM TBL_USER_AUTH WHERE USER_ID='${userId}';`;
+			}
+			mysqlConnection.query(sql, (e, r) => checkAuthToken(e, r));
+		} else {
+			sendRes(0, res, undefined, 'INVALID USERNAME OR PASSWORD');
+		}
+	};
+
+	// Check auth token of DB
+	const checkAuthToken = (err, result) => {
+		if (err) {
+			sendRes(-1, res);
+		} else {
+			// TODO create new validity datetime
+			let d = new Date();
+			d.setHours(d.getHours() + 24);
+			let validity = d.toISOString().slice(0, 19).replace('T', ' ');
+
+			// console.log('Cookie authToken: ',_authToken);
+			// console.log('Result len of tokens: ', result.length);
+			if (result.length === 0 && _authToken) {
+				res.clearCookie('userId').clearCookie('authToken');
+				sendRes(0, res, undefined, 'UNAUTHORISED');
+			} else {
+				if (!_authToken) {
+					console.log('Generating new token');
+					sql = `INSERT INTO TBL_USER_AUTH (USER_ID, VALIDITY) VALUES ('${userId}', '${validity}');`;
+				} else {
+					console.log('Updating token');
+					sql = `UPDATE TBL_USER_AUTH SET VALIDITY='${validity}' WHERE USER_ID='${userId}' AND AUTH_ID='${_authToken}';`;
+				}
+				mysqlConnection.query(sql, (e, r) => {
+					if (e) {
+						// console.log((e));
+						sendRes(-1, res);
+					} else {
+						// console.log(r);
+
+						res.cookie('userId', userId, {
+							sameSite: 'Lex',
+							httpOnly: true
+							// secure: true
+							// maxAge: 1000 * 60 * 60 * 2
+						}).cookie(
+							'authToken',
+							_authToken ? _authToken : r['insertId'],
+							{
+								sameSite: 'Lex',
+								httpOnly: true
+								// secure: true
+								// maxAge: 1000 * 60 * 60 * 2
+							}
+						);
+
+						let data = {
+							USER_ID: userId,
+							USER_NAME: userName
+						};
+						sendRes(1, res, data);
+					}
+				});
+			}
+			// else {
+			// 	sendRes(1, res)
+			// }
+		}
+	};
+
+	fetchUser();
+});
+
+router.get('/signOut', check, (req, res) => {
+	res.clearCookie('userId').clearCookie('authToken');
+	sendRes(1, res);
+});
+
+router.get('/check', check, (req, res) => {
+	data = {
+		USER_ID: parseInt(req.cookies.userId)
+	};
+	sendRes(1, res);
 });
 
 module.exports = router;
