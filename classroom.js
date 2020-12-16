0f1a161caabd3f2805bf993924679c16ed93d8f3
Prajwal-P <prajwal72p@gmail.com>
2020-12-17 00:47:47 +0530

    remove student modal pending

--- classroom.js
+++ classroom.js
@@ -1,7 +1,186 @@
 let baseURL = 'http://127.0.0.1:8888';
 let classID = -1;
 
-let classroom = {};
+let classroom, students;
+let page_toggle_btn, quiz_card_wrapper;
+let stu_table = '<h1>Loading...</h1>';
+
+function quiz_card_template(cl) {
+	cl.START_TIME = new Date(cl.START_TIME);
+	cl.END_TIME = new Date(cl.END_TIME);
+
+	let timezone = cl.START_TIME.getTimezoneOffset();
+	timezone *= 2;
+	let hr = parseInt(timezone / -60);
+	let min = timezone % -60;
+	min *= -1;
+	// console.log(hr, min);
+	cl.START_TIME.setHours(cl.START_TIME.getHours() + hr);
+	cl.START_TIME.setMinutes(cl.START_TIME.getMinutes() + min);
+	cl.END_TIME.setHours(cl.END_TIME.getHours() + hr);
+	cl.END_TIME.setMinutes(cl.END_TIME.getMinutes() + min);
+
+	let id1, id2, id3, id4, id5;
+	let cur_time = new Date();
+	let month = [
+		'JAN',
+		'FEB',
+		'MAR',
+		'APR',
+		'MAY',
+		'JUN',
+		'JUL',
+		'AUG',
+		'SEP',
+		'OCT',
+		'NOV',
+		'DEC'
+	];
+	if (classroom.CLASS_CODE) {
+		id1 = `<span class="material-icons delete_btn">delete</span>`;
+		id2 = 'Open Quiz';
+		id3 =
+			'Start Time: ' +
+			cl.START_TIME.getDate() +
+			' ' +
+			month[cl.START_TIME.getMonth()] +
+			' ' +
+			cl.START_TIME.getFullYear() +
+			' ' +
+			cl.START_TIME.toLocaleTimeString().replace(
+				/([\d]+:[\d]{2})(:[\d]{2})(.*)/,
+				'$1$3'
+			);
+		id4 =
+			'End Time: ' +
+			cl.END_TIME.getDate() +
+			' ' +
+			month[cl.END_TIME.getMonth()] +
+			' ' +
+			cl.END_TIME.getFullYear() +
+			' ' +
+			cl.END_TIME.toLocaleTimeString().replace(
+				/([\d]+:[\d]{2})(:[\d]{2})(.*)/,
+				'$1$3'
+			);
+	} else {
+		id1 = `MAX MARKS : ${cl.MAX_MARKS}`;
+		id2 = 'Take Quiz';
+		if (cur_time < cl.START_TIME) {
+			// Test still not started
+			id3 =
+				'Start Time: ' +
+				cl.START_TIME.getDate() +
+				' ' +
+				month[cl.START_TIME.getMonth()] +
+				' ' +
+				cl.START_TIME.getFullYear() +
+				' ' +
+				cl.START_TIME.toLocaleTimeString().replace(
+					/([\d]+:[\d]{2})(:[\d]{2})(.*)/,
+					'$1$3'
+				);
+			id4 =
+				'End Time: ' +
+				cl.END_TIME.getDate() +
+				' ' +
+				month[cl.END_TIME.getMonth()] +
+				' ' +
+				cl.END_TIME.getFullYear() +
+				' ' +
+				cl.END_TIME.toLocaleTimeString().replace(
+					/([\d]+:[\d]{2})(:[\d]{2})(.*)/,
+					'$1$3'
+				);
+			id5 = 'disabled';
+		} else if (cl.SCORE) {
+			// Test already taken
+			if (cur_time < cl.END_TIME) {
+				// Do not show score, Disable test button
+				id2 = 'Quiz Taken';
+				id3 = 'Duration: ' + cl.DURATION + ' min';
+				id4 = 'No. of Questions: ' + cl.NO_OF_QUESTIONS;
+				id5 = 'disabled';
+			} else {
+				// Show score
+				id2 = 'Score: ' + cl.SCORE;
+				id3 =
+					'Start Time: ' +
+					cl.START_TIME.getDate() +
+					' ' +
+					month[cl.START_TIME.getMonth()] +
+					' ' +
+					cl.START_TIME.getFullYear() +
+					' ' +
+					cl.START_TIME.toLocaleTimeString().replace(
+						/([\d]+:[\d]{2})(:[\d]{2})(.*)/,
+						'$1$3'
+					);
+				id4 =
+					'End Time: ' +
+					cl.END_TIME.getDate() +
+					' ' +
+					month[cl.END_TIME.getMonth()] +
+					' ' +
+					cl.END_TIME.getFullYear() +
+					' ' +
+					cl.END_TIME.toLocaleTimeString().replace(
+						/([\d]+:[\d]{2})(:[\d]{2})(.*)/,
+						'$1$3'
+					);
+				id5 = "class='btn_sec'";
+			}
+		} else {
+			// Test not taken
+			if (cur_time < cl.END_TIME) {
+				// Enable take test button
+				id3 = 'Duration: ' + cl.DURATION + ' min';
+				id4 = 'No. of Questions: ' + cl.NO_OF_QUESTIONS;
+			} else {
+				// Test Expired and disable button
+				id2 = 'Test Expired';
+				id3 =
+					'Start Time: ' +
+					cl.START_TIME.getDate() +
+					' ' +
+					month[cl.START_TIME.getMonth()] +
+					' ' +
+					cl.START_TIME.getFullYear() +
+					' ' +
+					cl.START_TIME.toLocaleTimeString().replace(
+						/([\d]+:[\d]{2})(:[\d]{2})(.*)/,
+						'$1$3'
+					);
+				id4 =
+					'End Time: ' +
+					cl.END_TIME.getDate() +
+					' ' +
+					month[cl.END_TIME.getMonth()] +
+					' ' +
+					cl.END_TIME.getFullYear() +
+					' ' +
+					cl.END_TIME.toLocaleTimeString().replace(
+						/([\d]+:[\d]{2})(:[\d]{2})(.*)/,
+						'$1$3'
+					);
+				id5 = "class='btn_ter' disabled";
+			}
+		}
+	}
+	return `
+						<div class="quiz_card">
+							<div class="quiz_name_and_marks">
+								<h2>${cl.TITLE}</h2>
+								<p>${id1}</p>
+								</div>
+							<P>${cl.DESCRIPTION}</P>
+							<div class="dur_and_no_of_que">
+								<p>${id3}</p>
+								<p>${id4}</p>
+								<button ${id5}>${id2}</button>
+							</div>
+						</div>`;
+}
 
 window.onload = () => {
 	let requestOptions = {
@@ -50,192 +229,59 @@ window.onload = () => {
 					options_card.style.display = 'block';
 					document.getElementById('classCode').innerHTML =
 						classroom.CLASS_CODE;
+
+					fetch(
+						`${baseURL}/student/allstudents/${classID}`,
+						requestOptions
+					)
+						.then(result => result.json())
+						.then(res => {
+							if (res.STATUS === 1) {
+								students = res.DATA;
+								if (students.length === 0)
+									stu_table =
+										'<h1>No Students in this Class</h1>';
+								else {
+									stu_table =
+										'<h1>STUDENTS</h1><table><tr><th>FIRST NAME</th><th>LAST NAME</th><th>EMAIL</th><th></th></tr>';
+									students.forEach((student, i) => {
+										stu_table += `<tr>
+											<td>${student.FIRST_NAME}</td>
+											<td>${student.LAST_NAME}</td>
+											<td>${student.EMAIL}</td>
+											<td><button onclick="deleteStudent(${i})">
+													<span class="material-icons">remove_circle</span>
+													REMOVE STUDENT
+											</button></td>
+										</tr>`;
+									});
+									stu_table += '</table>';
+								}
+							} else {
+								alert(res.MESSAGE);
+								window.location = 'dashboard.html';
+							}
+						})
+						.catch(error => console.log('error', error));
 				}
 
-				function quiz_card_template(cl) {
-					cl.START_TIME = new Date(cl.START_TIME);
-					cl.END_TIME = new Date(cl.END_TIME);
-
-					let timezone = cl.START_TIME.getTimezoneOffset();
-					timezone *= 2;
-					let hr = parseInt(timezone / -60);
-					let min = timezone % -60;
-					min *= -1;
-					// console.log(hr, min);
-					cl.START_TIME.setHours(cl.START_TIME.getHours() + hr);
-					cl.START_TIME.setMinutes(cl.START_TIME.getMinutes() + min);
-					cl.END_TIME.setHours(cl.END_TIME.getHours() + hr);
-					cl.END_TIME.setMinutes(cl.END_TIME.getMinutes() + min);
-
-					let id1, id2, id3, id4, id5;
-					let cur_time = new Date();
-					let month = [
-						'JAN',
-						'FEB',
-						'MAR',
-						'APR',
-						'MAY',
-						'JUN',
-						'JUL',
-						'AUG',
-						'SEP',
-						'OCT',
-						'NOV',
-						'DEC'
-					];
-					if (classroom.CLASS_CODE) {
-						id1 = `<span class="material-icons delete_btn">delete</span>`;
-						id2 = 'Open Quiz';
-						id3 =
-							'Start Time: ' +
-							cl.START_TIME.getDate() +
-							' ' +
-							month[cl.START_TIME.getMonth()] +
-							' ' +
-							cl.START_TIME.getFullYear() +
-							' ' +
-							cl.START_TIME.toLocaleTimeString().replace(
-								/([\d]+:[\d]{2})(:[\d]{2})(.*)/,
-								'$1$3'
-							);
-						id4 =
-							'End Time: ' +
-							cl.END_TIME.getDate() +
-							' ' +
-							month[cl.END_TIME.getMonth()] +
-							' ' +
-							cl.END_TIME.getFullYear() +
-							' ' +
-							cl.END_TIME.toLocaleTimeString().replace(
-								/([\d]+:[\d]{2})(:[\d]{2})(.*)/,
-								'$1$3'
-							);
-					} else {
-						id1 = `MAX MARKS : ${cl.MAX_MARKS}`;
-						id2 = 'Take Quiz';
-						if (cur_time < cl.START_TIME) {
-							// Test still not started
-							id3 =
-								'Start Time: ' +
-								cl.START_TIME.getDate() +
-								' ' +
-								month[cl.START_TIME.getMonth()] +
-								' ' +
-								cl.START_TIME.getFullYear() +
-								' ' +
-								cl.START_TIME.toLocaleTimeString().replace(
-									/([\d]+:[\d]{2})(:[\d]{2})(.*)/,
-									'$1$3'
-								);
-							id4 =
-								'End Time: ' +
-								cl.END_TIME.getDate() +
-								' ' +
-								month[cl.END_TIME.getMonth()] +
-								' ' +
-								cl.END_TIME.getFullYear() +
-								' ' +
-								cl.END_TIME.toLocaleTimeString().replace(
-									/([\d]+:[\d]{2})(:[\d]{2})(.*)/,
-									'$1$3'
-								);
-							id5 = 'disabled';
-						} else if (cl.SCORE) {
-							// Test already taken
-							if (cur_time < cl.END_TIME) {
-								// Do not show score, Disable test button
-								id2 = 'Quiz Taken';
-								id3 = 'Duration: ' + cl.DURATION + ' min';
-								id4 = 'No. of Questions: ' + cl.NO_OF_QUESTIONS;
-								id5 = 'disabled';
-							} else {
-								// Show score
-								id2 = 'Score: ' + cl.SCORE;
-								id3 =
-									'Start Time: ' +
-									cl.START_TIME.getDate() +
-									' ' +
-									month[cl.START_TIME.getMonth()] +
-									' ' +
-									cl.START_TIME.getFullYear() +
-									' ' +
-									cl.START_TIME.toLocaleTimeString().replace(
-										/([\d]+:[\d]{2})(:[\d]{2})(.*)/,
-										'$1$3'
-									);
-								id4 =
-									'End Time: ' +
-									cl.END_TIME.getDate() +
-									' ' +
-									month[cl.END_TIME.getMonth()] +
-									' ' +
-									cl.END_TIME.getFullYear() +
-									' ' +
-									cl.END_TIME.toLocaleTimeString().replace(
-										/([\d]+:[\d]{2})(:[\d]{2})(.*)/,
-										'$1$3'
-									);
-								id5 = "class='btn_sec'";
-							}
-						} else {
-							// Test not taken
-							if (cur_time < cl.END_TIME) {
-								// Enable take test button
-								id3 = 'Duration: ' + cl.DURATION + ' min';
-								id4 = 'No. of Questions: ' + cl.NO_OF_QUESTIONS;
-							} else {
-								// Test Expired and disable button
-								id2 = 'Test Expired';
-								id3 =
-									'Start Time: ' +
-									cl.START_TIME.getDate() +
-									' ' +
-									month[cl.START_TIME.getMonth()] +
-									' ' +
-									cl.START_TIME.getFullYear() +
-									' ' +
-									cl.START_TIME.toLocaleTimeString().replace(
-										/([\d]+:[\d]{2})(:[\d]{2})(.*)/,
-										'$1$3'
-									);
-								id4 =
-									'End Time: ' +
-									cl.END_TIME.getDate() +
-									' ' +
-									month[cl.END_TIME.getMonth()] +
-									' ' +
-									cl.END_TIME.getFullYear() +
-									' ' +
-									cl.END_TIME.toLocaleTimeString().replace(
-										/([\d]+:[\d]{2})(:[\d]{2})(.*)/,
-										'$1$3'
-									);
-								id5 = "class='btn_ter' disabled";
-							}
-						}
-					}
-					return `
-						<div class="quiz_card">
-							<div class="quiz_name_and_marks">
-								<h2>${cl.TITLE}</h2>
-								<p>${id1}</p>
-								</div>
-							<P>${cl.DESCRIPTION}</P>
-							<div class="dur_and_no_of_que">
-								<p>${id3}</p>
-								<p>${id4}</p>
-								<button ${id5}>${id2}</button>
-							</div>
-						</div>`;
-				}
-
-				document.querySelector('.quiz_card_wrapper').innerHTML = `
+				quiz_card_wrapper = document.querySelector(
+					'.quiz_card_wrapper'
+				);
+				if (classroom.QUIZZES.length === 0) {
+					quiz_card_wrapper.innerHTML = `<h1>NO QUIZZES IN THIS CLASS</h1>`;
+				} else {
+					quiz_card_wrapper.innerHTML = `
 				${classroom.QUIZZES.map(quiz_card_template).join('')}`;
+				}
 			} else {
 				alert(res.MESSAGE);
 				window.location = 'dashboard.html';
 			}
-		});
+		})
+		.catch(error => console.log('error', error));
+
+	page_toggle_btn = document.getElementById('page_toggle_btn');
 };
 
 let menu_visible = false;
@@ -317,34 +363,23 @@ deleteClassForm.addEventListener('submit', e => {
 	}
 });
 
-const allStudents = () => {
-	let quiz_card_wrapper = document.querySelector('.quiz_card_wrapper');
-	quiz_card_wrapper.innerHTML = '<h1>Loading...</h1>';
-	let requestOptions = {
-		method: 'GET',
-		mode: 'cors',
-		credentials: 'include'
-	};
-	fetch(`${baseURL}/student/allstudents/${classID}`, requestOptions)
-		.then(result => result.json())
-		.then(res => {
-			if (res.STATUS === 1) {
-				let students = res.DATA;
-				if (students.length === 0)
-					quiz_card_wrapper.innerHTML =
-						'<h1>No Students in this Class</h1>';
-				else {
-					let stu_table =
-						'<table><tr><th>FIRST NAME</th><th>LAST NAME</th><th>EMAIL</th></tr>';
-					for (let student of students) {
-						stu_table += `<tr><td>${student.FIRST_NAME}</td><td>${student.LAST_NAME}</td><td>${student.EMAIL}</td></tr>`;
-					}
-					stu_table += '</table>';
-					quiz_card_wrapper.innerHTML = stu_table;
-				}
-			} else {
-				alert(res.MESSAGE);
-				window.location = 'dashboard.html';
-			}
-		});
+let page = 1;
+const toggle_contents = () => {
+	if (page === 1) {
+		quiz_card_wrapper.innerHTML = stu_table;
+		page_toggle_btn.innerHTML =
+			'<span class="material-icons">assignment</span> Quizzes';
+		page = 2;
+	} else {
+		quiz_card_wrapper.innerHTML = `${classroom.QUIZZES.map(
+			quiz_card_template
+		).join('')}`;
+		page_toggle_btn.innerHTML =
+			'<span class="material-icons">people</span> Students';
+		page = 1;
+	}
+};
+
+const deleteStudent = i => {
+	console.log(students[i]);
 };
