c513253710b75feed3328aa15800f894a45700d3
Prajwal-P <prajwal72p@gmail.com>
2020-12-17 00:47:31 +0530

    remove student api

--- server/Routes/student.js
+++ server/Routes/student.js
@@ -48,4 +48,47 @@ router.get('/allstudents/:classID', check, (req, res) => {
 	isTeacher();
 });
 
+router.delete('/remove', check, (req, res) => {
+	const classID = req.query['classID'];
+	const studentID = req.query['studentID'];
+	const userID = req.cookies.userId;
+
+	const isTeacher = () => {
+		sql = `SELECT * FROM TBL_CLASSROOM WHERE ID='${classID}' AND TEACHER_ID='${userID}';`;
+		mysqlConnection.query(sql, (err, result) => {
+			if (err) {
+				sendRes(-1, res);
+			} else {
+				if (result.length === 0) {
+					sendRes(
+						0,
+						res,
+						undefined,
+						'YOU ARE NOT AUTHORISED FOR THIS ACTION'
+					);
+				} else deleteStudent();
+			}
+		});
+	};
+
+	const deleteStudent = () => {
+		sql = `DELETE FROM TBL_STUDENT_CLASSROOM WHERE CLASS_ID='${classID}' AND STUDENT_ID='${studentID}';`;
+		mysqlConnection.query(sql, (err, result) => {
+			if (err) {
+				sendRes(-1, res);
+			} else {
+				if (result['affectedRows'] === 1)
+					sendRes(1, res, undefined, 'STUDENT REMOVED SUCCESSFULLY');
+				else sendRes(1, res, undefined, 'STUDENT COULD NOT BE REMOVED');
+			}
+		});
+	};
+
+	if (classID && studentID) {
+		isTeacher();
+	} else {
+		sendRes(0, res, undefined, 'INSUFFICIENT DATA');
+	}
+});
+
 module.exports = router;
