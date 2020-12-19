154d7db8253c9a27c78398ebe53e705099e24bce
Revanthpn <revanthpn2000@gmail.com>
2020-12-19 17:37:18 +0530

    Addquestion functionality v1

--- addquiz.js
+++ addquiz.js
@@ -9,3 +9,27 @@ function toggle_dropdown() {
 		menu_visible = true;
 	}
 }
+
+function addQuestion() {
+	let node = document.createElement('div');
+	node.className = 'question_options';
+	node.innerHTML = `<label for="quetion">Question : </label>
+	<span class="material-icons">delete</span>
+	<input class="question" type="text" name="question" required>
+	<label for="mark">Mark(s) : </label>
+	<input class="question" type="number" style="width: 100px;" name="mark" required>
+	<br>
+	<label for="options">Options : </label><br>
+	<input class="checks" type="radio" name="option" required>
+	<input class="txt_field" style="margin: 4px 16px 0 0;" type="text" name="option_1" required>
+	<input class="checks" type="radio" name="option">
+	<input class="txt_field" type="text" name="option_2" required>
+	<input class="checks" type="radio" name="option">
+	<input class="txt_field" style="margin-right: 16px;" type="text" name="option_3" required>
+	<input class="checks" type="radio" name="option">
+	<input class="txt_field" type="text" name="option_4" required>`;
+
+	document.getElementById('addQuizForm').appendChild(node);
+}
+
+function deleteQuestion() {}
