5faec377180f7a9ce71557f57c92291239695d7a
Revanthpn <revanthpn2000@gmail.com>
2020-12-21 15:01:26 +0530

    quiz page done

--- quiz.js
+++ quiz.js
@@ -9,3 +9,24 @@ function toggle_dropdown() {
 		menu_visible = true;
 	}
 }
+
+const start_minutes = 120;
+let time = start_minutes * 60;
+
+const count_down = document.getElementById('countdown');
+
+setInterval(updateCountdown, 1000);
+
+function updateCountdown() {
+	let hours = Math.floor(time / 3600);
+	let minutes = Math.floor(time / 60);
+	minutes %= 60;
+	let seconds = time % 60;
+
+	hours = hours < 10 ? '0' + hours : hours;
+	minutes = minutes < 10 ? '0' + minutes : minutes;
+	seconds = seconds < 10 ? '0' + seconds : seconds;
+
+	count_down.innerHTML = `${hours} : ${minutes} : ${seconds}`;
+	time--;
+}
