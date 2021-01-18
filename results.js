2548ee524a2c391ce180adb34011821c10805c54
Revanthpn <revanthpn2000@gmail.com>
2021-01-19 00:47:13 +0530

    Results page started

---
+++ results.js
@@ -0,0 +1,12 @@
+let menu_visible = false;
+function toggle_dropdown() {
+	// let dd = document.getElementById("dropdown");
+	let dd = document.querySelector('.dd_wrapper');
+	if (menu_visible) {
+		dd.classList.remove('show');
+		menu_visible = false;
+	} else {
+		dd.classList.add('show');
+		menu_visible = true;
+	}
+}
